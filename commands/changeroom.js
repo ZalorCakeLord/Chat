e811dfbe961be4fe488491f38061731c43c47ab5
Robert Chavana <59065215+ZalorCakeLord@users.noreply.github.com>
2022-02-19 13:03:55 -0600

    Update changeroom.js

--- commands/changeroom.js
+++ commands/changeroom.js
@@ -1,8 +1,13 @@
 //rooms are gonna suck ass lol
 //idk how I'mma do this, gonna have to change the basic message logic.
 //I fuckin did it
+
   var room = args[0]
   SOCKET_ADDY[user.username].room = args[0]
   user.room = args[0]
+  if(args[0] === overwatchroom){
+    systemmessage(usersocket,`Welcome to the Overwatch room, ${user.username}`)
+    SOCKET_LIST[usersocket].emit('changeroom','<h3 style="display:inline"class="glowUnaligned">Overwatch</h3>')
+  }else{
   systemmessage(usersocket,`Your room has been changed to ${args[0]}`)
-  SOCKET_LIST[usersocket].emit('changeroom',room)
+  SOCKET_LIST[usersocket].emit('changeroom',room)}
